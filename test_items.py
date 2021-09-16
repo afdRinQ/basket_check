b6db3da6cdbe723b17022ae8f79e6375a683f49b
afdRinQ <90555273+afdRinQ@users.noreply.github.com>
2021-09-16 15:04:02 +0500

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_basket_check(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert len(browser.find_element_by_css_selector('button.btn-add-to-basket').get_attribute(
+        'value')) != 0, "Add to cart button doesn't exist!"
